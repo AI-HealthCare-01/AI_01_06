7850d516573b258d440b7e56ba00211c694cc6b8
Hu <kimth8778@gmail.com>
2026-03-16 02:38:56 +0900

    feat(worker): 소프트 삭제 30일 경과 계정 물리 삭제 배치 (ARQ cron)

--- ai_worker/tests/conftest.py
+++ ai_worker/tests/conftest.py
@@ -9,7 +9,14 @@ async def setup_db():
     await Tortoise.init(
         db_url=TEST_DB_URL,
         modules={
-            "models": ["app.models.user", "app.models.prescription", "app.models.guide", "app.models.patient_profile"]
+            "models": [
+                "app.models.user",
+                "app.models.prescription",
+                "app.models.guide",
+                "app.models.patient_profile",
+                "app.models.auth_provider",
+                "app.models.terms_consent",
+            ]
         },
     )
     await Tortoise.generate_schemas()
