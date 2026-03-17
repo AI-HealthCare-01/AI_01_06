87cef02049e2190aa8e10883cad36aa15157ee7a
whibenchanter <whibenchanter@gmail.com>
2026-03-17 17:52:06 +0900

    fix: fromatiing

--- app/app/models/refresh_token.py
+++ app/app/models/refresh_token.py
@@ -5,9 +5,7 @@ from tortoise.models import Model
 class RefreshToken(Model):
     id = fields.IntField(primary_key=True)
     token = fields.CharField(max_length=128, unique=True, index=True)
-    user: fields.ForeignKeyRelation = fields.ForeignKeyField(
-        "models.User", related_name="refresh_tokens"
-    )
+    user: fields.ForeignKeyRelation = fields.ForeignKeyField("models.User", related_name="refresh_tokens")
     expires_at = fields.DatetimeField()
     revoked = fields.BooleanField(default=False)
     created_at = fields.DatetimeField(auto_now_add=True)
